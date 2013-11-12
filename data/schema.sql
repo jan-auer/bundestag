d3b628e7a93a15da0930f3e4d68e960b747cb7ab
Manuel Gerding <me@manuel-gerding.de>
2013-11-12 09:18:04 +0100

    updated data model for consistency between relational model and conceptual model

--- data/schema.sql
+++ data/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE election 
 (
 	id 		SERIAL PRIMARY KEY,
-	number 	INTEGER,
+	number 	INTEGER NOT NULL,
 	date 	DATE
 );
 
@@ -10,7 +10,15 @@ CREATE TABLE voter
 	id 			SERIAL PRIMARY KEY,
 	name 		VARCHAR,
 	birthday 	DATE,
-	election_id INTEGER NOT NULL REFERENCES election(id)
+	election_id INTEGER NOT NULL REFERENCES election(id) on delete cascade on update cascade
+);
+
+CREATE TABLE state
+(
+	id 			SERIAL PRIMARY KEY,
+	name 		VARCHAR,
+	population 	INTEGER,
+	election_id INTEGER NOT NULL REFERENCES election(id) on delete cascade on update cascade
 );
 
 CREATE TABLE party
@@ -27,32 +35,49 @@ CREATE TABLE party
 CREATE TABLE state_list
 (
 	id 			SERIAL PRIMARY KEY,
-	election_id	INTEGER NOT NULL REFERENCES election(id),
-	party_id	INTEGER NOT NULL REFERENCES party(id)
+	state_id	INTEGER NOT NULL REFERENCES state(id) on delete cascade on update cascade,
+	party_id	INTEGER NOT NULL REFERENCES party(id) on delete cascade on update cascade
 );
 
 CREATE TABLE candidate
 (
 	id 		 SERIAL PRIMARY KEY,
 	name 	 VARCHAR,
-	birthday DATE
+	birthday DATE,
+	party_id INTEGER REFERENCES party(id)
 );
 
-CREATE TABLE state_candidate
+CREATE TABLE state_candidacy
 (
-	state_list_id	INTEGER NOT NULL REFERENCES state_list(id),
-	candidate_id	INTEGER NOT NULL REFERENCES candidate(id),
-	position 		INTEGER,
+	state_list_id	INTEGER NOT NULL REFERENCES state_list(id) on delete cascade on update cascade,
+	candidate_id	INTEGER NOT NULL REFERENCES candidate(id) on delete cascade on update cascade,
+	position 		INTEGER NOT NULL,
 	
 	PRIMARY KEY (state_list_id, candidate_id)
 );
 
+CREATE TABLE constituency
+(
+	id 			SERIAL PRIMARY KEY,
+	number 		INTEGER NOT NULL,
+	name 		VARCHAR,
+	state_id	INTEGER NOT NULL REFERENCES state(id) on delete cascade on update cascade
+);
+
 CREATE TABLE constituency_candidacy
 (
-	id 			 SERIAL PRIMARY KEY,
-	election_id  INTEGER NOT NULL REFERENCES election(id),
-	party_id	 INTEGER NULL REFERENCES party(id),
-	candidate_id INTEGER NOT NULL REFERENCES candidate(id)
+	id 				SERIAL PRIMARY KEY,
+	constituency_id INTEGER NOT NULL REFERENCES constituency(id) on delete cascade on update cascade,
+	candidate_id 	INTEGER NOT NULL REFERENCES candidate(id),
+	
+	unique(constituency_id, candidate_id)
+);
+
+CREATE TABLE site
+(
+	id 	 			SERIAL PRIMARY KEY,
+	name 			VARCHAR,
+	constituency_id INTEGER NOT NULL REFERENCES constituency(id) on delete cascade on update cascade
 );
 
 CREATE TABLE result_type
@@ -61,50 +86,23 @@ CREATE TABLE result_type
 	name VARCHAR
 );
 
-CREATE TABLE site
-(
-	id 	 SERIAL PRIMARY KEY,
-	name VARCHAR
-);
-
 CREATE TABLE result
 (
 	id				SERIAL PRIMARY KEY,
-	count			INTEGER,
 	result_type_id	INTEGER NOT NULL REFERENCES result_type(id),
 	site_id			INTEGER NOT NULL REFERENCES site(id)
 );
 
 CREATE TABLE first_result
 (
+	id 							SERIAL PRIMARY KEY,
 	result_id 				  INTEGER NOT NULL REFERENCES result(id),
 	constituency_candidacy_id INTEGER NOT NULL REFERENCES constituency_candidacy(id)
 );
 
 CREATE TABLE second_result
 (
+	id 				SERIAL PRIMARY KEY,
 	result_id 	  	INTEGER NOT NULL REFERENCES result(id),
 	state_list_id 	INTEGER NOT NULL REFERENCES state_list(id)
-);
-
-CREATE TABLE state
-(
-	id 			SERIAL PRIMARY KEY,
-	name 		VARCHAR,
-	population 	INTEGER
-);
-
-CREATE TABLE constituency
-(
-	state_id	INTEGER NOT NULL REFERENCES state(id),
-	number 		INTEGER,
-	name 		VARCHAR,
-	
-	PRIMARY KEY(state_id, number)
-);
-
-CREATE TABLE district
-(
-	id 		INTEGER,
-	name 	VARCHAR
 );
