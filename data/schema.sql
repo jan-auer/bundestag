3791cab1231f13039c40bc5f3389175d32717100
Manuel Gerding <me@manuel-gerding.de>
2013-11-16 07:46:19 +0100

    fixed bug of parties inserted as free candidate

--- data/schema.sql
+++ data/schema.sql
@@ -77,8 +77,8 @@ CREATE TABLE first_result
 
 CREATE TABLE second_result
 (
-  id            SERIAL PRIMARY KEY,
-  state_list_id INTEGER NOT NULL REFERENCES state_list (id),
+  id              SERIAL PRIMARY KEY,
+  state_list_id   INTEGER NOT NULL REFERENCES state_list (id),
   constituency_id INTEGER NOT NULL REFERENCES constituency (id)
 );
 CREATE TABLE aggregated_first_result
@@ -90,8 +90,8 @@ CREATE TABLE aggregated_first_result
 
 CREATE TABLE aggregated_second_result
 (
-  id            SERIAL PRIMARY KEY,
-  state_list_id INTEGER NOT NULL REFERENCES state_list (id),
+  id              SERIAL PRIMARY KEY,
+  state_list_id   INTEGER NOT NULL REFERENCES state_list (id),
   constituency_id INTEGER NOT NULL REFERENCES constituency (id),
-  count         INTEGER
+  count           INTEGER
 );
--- sources/src/Btw/Bundle/ImporterBundle/Import/Importer.php
+++ sources/src/Btw/Bundle/ImporterBundle/Import/Importer.php
@@ -139,16 +139,16 @@ class Importer
 
 				for ($i = 19; $i < count($row) - 2; $i += 4) {
 					$partyAbbr = $data[0][$i];
-					if($partyAbbr=='Übrige') continue;
+					if ($partyAbbr == 'Übrige') continue;
 					$firstresult = $row[$i];
 					$secondresult = $row[$i + 2];
 
-						$stateList = $this->factory->createStateList($stateName, $partyAbbr);
-						$this->em->persist($stateList);
-					}
+					$stateList = $this->factory->createStateList($stateName, $partyAbbr);
+					$this->em->persist($stateList);
 				}
 			}
 		}
+	}
 
 	private function importCandidates(array &$data)
 	{
@@ -171,7 +171,7 @@ class Importer
 	private function importFreeCandidates(Election $election, array $parties, array $constituencies)
 	{
 		foreach ($constituencies as $constituency) {
-			$electionNo =date("y", $election->getDate()->getTimestamp());
+			$electionNo = date("y", $election->getDate()->getTimestamp());
 			$stateNo = str_pad($constituency->getState()->getNumber(), 2, '0', STR_PAD_LEFT);
 			$constituencyNo = str_pad($constituency->getNumber(), 3, '0', STR_PAD_LEFT);
 
@@ -185,6 +185,7 @@ class Importer
 
 			foreach ($parties as $party) {
 				unset($results[$party->getName()]);
+				unset($results[$party->getAbbreviation()]);
 			}
 
 			foreach ($results as $name => $votes) {
@@ -210,7 +211,7 @@ class Importer
 			if ($rowI++ < 3 || $rowI == $rowCount || (count($row) == 1 && is_null($row[0]))) continue; // skip first three rows, the last row and skip empty rows
 
 			$stateNo = $row[2];
-			if ($stateNo == 99) continue;
+			if ($stateNo == 99) continue; //skip states
 
 			$constituencyNo = $row[0];
 
@@ -222,20 +223,19 @@ class Importer
 				if ($firstResultCount > 0) {
 					$aggrFirstResult = $this->factory->createAggregatedFirstResultRow($constituencyNo, $party, $firstResultCount);
 					$this->em->persist($aggrFirstResult);
-	}
+				}
 
 				//secondresults for party
 				$secondResultCount = $row[$column + 2];
 				if ($secondResultCount > 0) {
 					$aggrSecondResult = $this->factory->createAggregatedSecondResult($party, $stateNo, $constituencyNo, $secondResultCount);
 					$this->em->persist($aggrSecondResult);
-}
+				}
 
 			}
 		}
 
 		//free candidates
-		$k = 0;
 		foreach ($this->freeConstituencyCandidateResults as $freeConstituencyCandidateResults) {
 			$freeConstituencyCandidate = $freeConstituencyCandidateResults[0];
 			$votes = $freeConstituencyCandidateResults[1];
