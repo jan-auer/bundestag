eb112fdf58db5f707cbe7cd138f9d8eba53ad6e5
Jan Michael Auer <jan.auer@me.com>
2013-12-05 18:05:39 +0100

    Update schema.sql: Use fully qualified identifiers.

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,30 +1,30 @@
 CREATE TABLE election
 (
-  id     SERIAL PRIMARY KEY,
-  number INTEGER NOT NULL,
-  date   DATE    NOT NULL
+  election_id SERIAL PRIMARY KEY,
+  number      INTEGER NOT NULL,
+  date        DATE    NOT NULL
 );
 
 CREATE TABLE voter
 (
-  id          SERIAL PRIMARY KEY,
+  voter_id    SERIAL PRIMARY KEY,
   name        TEXT    NOT NULL,
   birthday    DATE    NOT NULL,
-  election_id INTEGER NOT NULL REFERENCES election (id) ON DELETE CASCADE
+  election_id INTEGER NOT NULL REFERENCES election (election_id) ON DELETE CASCADE
 );
 
 CREATE TABLE state
 (
-  id          SERIAL PRIMARY KEY,
+  state_id    SERIAL PRIMARY KEY,
   number      INTEGER NOT NULL,
   name        TEXT    NOT NULL,
   population  INTEGER NOT NULL,
-  election_id INTEGER NOT NULL REFERENCES election (id) ON DELETE CASCADE
+  election_id INTEGER NOT NULL REFERENCES election (election_id) ON DELETE CASCADE
 );
 
 CREATE TABLE party
 (
-  id                      SERIAL PRIMARY KEY,
+  party_id                SERIAL PRIMARY KEY,
   name                    TEXT    NOT NULL,
   abbreviation            TEXT,
   formation_date          DATE,
@@ -35,63 +35,64 @@ CREATE TABLE party
 
 CREATE TABLE state_list
 (
-  id       SERIAL PRIMARY KEY,
-  state_id INTEGER NOT NULL REFERENCES state (id) ON DELETE CASCADE,
-  party_id INTEGER NOT NULL REFERENCES party (id) ON DELETE CASCADE
+  state_list_id SERIAL PRIMARY KEY,
+  state_id      INTEGER NOT NULL REFERENCES state (state_id) ON DELETE CASCADE,
+  party_id      INTEGER NOT NULL REFERENCES party (party_id) ON DELETE CASCADE
 );
 
 CREATE TABLE candidate
 (
-  id       SERIAL PRIMARY KEY,
-  name     TEXT NOT NULL,
-  birthday DATE,
-  party_id INTEGER REFERENCES party (id)
+  candidate_id SERIAL PRIMARY KEY,
+  name         TEXT NOT NULL,
+  birthday     DATE,
+  party_id     INTEGER REFERENCES party (party_id)
 );
 
 CREATE TABLE state_candidacy
 (
-  candidate_id  INTEGER PRIMARY KEY REFERENCES candidate (id) ON DELETE CASCADE,
-  state_list_id INTEGER NOT NULL REFERENCES state_list (id) ON DELETE CASCADE,
+  candidate_id  INTEGER PRIMARY KEY REFERENCES candidate (candidate_id) ON DELETE CASCADE,
+  state_list_id INTEGER NOT NULL REFERENCES state_list (state_list_id) ON DELETE CASCADE,
   position      INTEGER NOT NULL
 );
 
 CREATE TABLE constituency
 (
-  id       SERIAL PRIMARY KEY,
-  number   INTEGER NOT NULL,
-  name     TEXT,
-  state_id INTEGER NOT NULL REFERENCES state (id) ON DELETE CASCADE
+  constituency_id SERIAL PRIMARY KEY,
+  number          INTEGER NOT NULL,
+  name            TEXT,
+  state_id        INTEGER NOT NULL REFERENCES state (state_id) ON DELETE CASCADE
 );
 
 CREATE TABLE constituency_candidacy
 (
-  candidate_id    INTEGER PRIMARY KEY REFERENCES candidate (id) ON DELETE CASCADE,
-  constituency_id INTEGER NOT NULL REFERENCES constituency (id) ON DELETE CASCADE
+  candidate_id    INTEGER PRIMARY KEY REFERENCES candidate (candidate_id) ON DELETE CASCADE,
+  constituency_id INTEGER NOT NULL REFERENCES constituency (constituency_id) ON DELETE CASCADE
 );
 
 CREATE TABLE first_result
 (
-  id           SERIAL PRIMARY KEY,
-  candidate_id INTEGER NOT NULL REFERENCES constituency_candidacy (candidate_id)
+  first_result_id SERIAL PRIMARY KEY,
+  candidate_id    INTEGER NOT NULL REFERENCES constituency_candidacy (candidate_id)
 );
 
 CREATE TABLE second_result
 (
-  id              SERIAL PRIMARY KEY,
-  state_list_id   INTEGER NOT NULL REFERENCES state_list (id),
-  constituency_id INTEGER NOT NULL REFERENCES constituency (id)
+  second_result_id SERIAL PRIMARY KEY,
+  state_list_id    INTEGER NOT NULL REFERENCES state_list (state_list_id),
+  constituency_id  INTEGER NOT NULL REFERENCES constituency (constituency_id)
 );
+
 CREATE TABLE aggregated_first_result
 (
-  id           SERIAL PRIMARY KEY,
-  candidate_id INTEGER NOT NULL REFERENCES constituency_candidacy (candidate_id),
-  count        INTEGER
+  aggregated_first_result_id SERIAL PRIMARY KEY,
+  candidate_id               INTEGER NOT NULL REFERENCES constituency_candidacy (candidate_id),
+  count                      INTEGER
 );
 
 CREATE TABLE aggregated_second_result
 (
-  id              SERIAL PRIMARY KEY,
-  state_list_id   INTEGER NOT NULL REFERENCES state_list (id),
-  constituency_id INTEGER NOT NULL REFERENCES constituency (id),
-  count           INTEGER
+  aggregated_second_result_id SERIAL PRIMARY KEY,
+  state_list_id               INTEGER NOT NULL REFERENCES state_list (state_list_id),
+  constituency_id             INTEGER NOT NULL REFERENCES constituency (constituency_id),
+  count                       INTEGER
 );
