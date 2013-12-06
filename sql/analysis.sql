1afbb8791e5b06ffac911f140ad0abdb1a3cfc65
Manuel Gerding <me@manuel-gerding.de>
2013-12-06 19:10:23 +0100

    Added begin of Q5

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -71,4 +71,25 @@ SELECT election_id, constituency_id, fv.party_id AS firstVotePartyId, sv.party_i
 FROM constituency_winners_first_votes fv JOIN constituency_winners_second_votes sv
   USING (election_id, constituency_id);
 -- ===================================================================
--- Q5
+-- Q5 USE RANKING FOR TOP X
+CREATE OR REPLACE VIEW close_constituency_winners (party_id, constituency_id, candidate_id, ranking) AS (
+    WITH constituency_second_winners (constituency_id, candidate_id, count) AS (
+        SELECT r1.constituency_id, r1.candidate_id, r1.count
+        FROM candidate_results r1
+        WHERE (SELECT COUNT(*)
+               FROM candidate_results r2
+               WHERE r1.constituency_id = r2.constituency_id AND r1.count < r2.count) = 1
+    ), constituency_winners_votes_diff (constituency_id, candidate_id, party_id, votes_diff) AS (
+        SELECT w.constituency_id, w.candidate_id, party_id, w.count - sw.count
+        FROM constituency_winners w JOIN candidate USING (candidate_id)
+          JOIN constituency_second_winners sw
+          USING (constituency_id)
+    )
+
+    SELECT party_id, constituency_id, candidate_id, ranking
+    FROM (SELECT party_id, constituency_id, candidate_id, row_number()
+                                                          OVER (PARTITION BY party_id
+                                                            ORDER BY votes_diff DESC) AS ranking
+          FROM party
+            JOIN constituency_winners_votes_diff USING (party_id)) t
+);
