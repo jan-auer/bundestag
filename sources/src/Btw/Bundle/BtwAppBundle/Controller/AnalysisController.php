3379529c5badf51d6a0803e54ac5eb410c3ac39a
centigradephilip <phschaef@googlemail.com>
2013-12-06 19:00:06 +0100

    Integrated data provider

--- sources/src/Btw/Bundle/BtwAppBundle/Controller/AnalysisController.php
+++ sources/src/Btw/Bundle/BtwAppBundle/Controller/AnalysisController.php
@@ -6,8 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AnalysisController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('BtwAppBundle:Analysis:index.html.twig', array());
-    }
+	public function indexAction()
+	{
+		$dataService = $this->get('btw_analysis.overview');
+		return $this->render('BtwAppBundle:Analysis:index.html.twig', array('year' => $dataService->getLatestElectionYear(),
+			'all_years' => $dataService->getAllElectionYears(),
+			'latest_result' => $dataService->getLatestElectionResults()));
+	}
 }
