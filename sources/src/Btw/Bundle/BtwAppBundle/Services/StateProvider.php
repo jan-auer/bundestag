d41931db2328eb8c3456f0b37acd0aed46855b99
centigradephilip <phschaef@googlemail.com>
2013-12-07 19:46:13 +0100

    Real data instead of dummy

--- sources/src/Btw/Bundle/BtwAppBundle/Services/StateProvider.php
+++ sources/src/Btw/Bundle/BtwAppBundle/Services/StateProvider.php
@@ -24,9 +24,13 @@ class StateProvider {
 
 	public function getStatesFor($year)
 	{
-		/** DUMMY */
-		return array(array('name' => 'Bayern', 'population' => 7219821, 'participation' => 60),
-					 array('name' => 'NRW', 'population' => 83278948392, 'participation' => 40),
-					array('name' => 'Saarland', 'population' => 3324, 'paticipation' => 20));
+		$electionsRepository = $this->em->getRepository('BtwPersistenceBundle:Election');
+		$elections = $electionsRepository->findAll();
+		$desiredElection = null;
+		foreach($elections as $election) if(date('Y', $election->getDate()->getTimestamp()) == $year) $desiredElection = $election;
+
+		$statesRepository = $this->em->getRepository('BtwPersistenceBundle:State');
+		$states = $statesRepository->findBy(array('election' => $desiredElection));
+		return $states;
 	}
 } 
