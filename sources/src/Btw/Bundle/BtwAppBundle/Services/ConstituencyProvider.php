7e6a25dee1cdb0f62b22f13663257d0425445c66
centigradephilip <phschaef@googlemail.com>
2013-12-13 18:24:21 +0100

    Removed obsolete function

--- sources/src/Btw/Bundle/BtwAppBundle/Services/ConstituencyProvider.php
+++ sources/src/Btw/Bundle/BtwAppBundle/Services/ConstituencyProvider.php
@@ -35,15 +35,6 @@ class ConstituencyProvider
 		return $this->em->find('BtwPersistenceBundle:Constituency', $id);
 	}
 
-	public function getAllForElection($election)
-	{
-		$query = $this->em->createQuery('SELECT c
-								FROM Btw\Bundle\PersistenceBundle\Entity\Constituency c JOIN c.state s
-								WHERE s.election = :election');
-		$query->setParameter('election', $election);
-		return $query->getResult();
-	}
-
 	public function getAllDetailsForElection($election)
 	{
 		$constituencies = array();
