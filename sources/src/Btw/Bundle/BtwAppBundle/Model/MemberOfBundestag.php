5729baf66975d8c52676739015f8952e51ff6a4d
centigradephilip <phschaef@googlemail.com>
2013-12-13 16:12:14 +0100

    Added further info to model according to recent changes to JSON interface

--- sources/src/Btw/Bundle/BtwAppBundle/Model/MemberOfBundestag.php
+++ sources/src/Btw/Bundle/BtwAppBundle/Model/MemberOfBundestag.php
@@ -15,6 +15,78 @@ class MemberOfBundestag {
 
 	private $name;
 
+	private $candidateId;
+
+	private $stateId;
+
+	private $constituencyId;
+
+	private $partyId;
+
+	/**
+	 * @param mixed $candidateId
+	 */
+	public function setCandidateId($candidateId)
+	{
+		$this->candidateId = $candidateId;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getCandidateId()
+	{
+		return $this->candidateId;
+	}
+
+	/**
+	 * @param mixed $constituencyId
+	 */
+	public function setConstituencyId($constituencyId)
+	{
+		$this->constituencyId = $constituencyId;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getConstituencyId()
+	{
+		return $this->constituencyId;
+	}
+
+	/**
+	 * @param mixed $partyId
+	 */
+	public function setPartyId($partyId)
+	{
+		$this->partyId = $partyId;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getPartyId()
+	{
+		return $this->partyId;
+	}
+
+	/**
+	 * @param mixed $stateId
+	 */
+	public function setStateId($stateId)
+	{
+		$this->stateId = $stateId;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getStateId()
+	{
+		return $this->stateId;
+	}
+
 	private $partyAbbreviation;
 
 	private $isDirect;
