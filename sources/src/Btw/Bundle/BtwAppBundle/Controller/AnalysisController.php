41cad46699555defc4709b5a70a22a8917ff3c0d
Jan Michael Auer <jan.auer@me.com>
2013-12-13 23:21:42 +0100

    Refactor AnalysisController and index.html.twig

--- sources/src/Btw/Bundle/BtwAppBundle/Controller/AnalysisController.php
+++ sources/src/Btw/Bundle/BtwAppBundle/Controller/AnalysisController.php
@@ -2,42 +2,81 @@
 
 namespace Btw\Bundle\BtwAppBundle\Controller;
 
+use Btw\Bundle\BtwAppBundle\Model\PartyResult;
+use Btw\Bundle\BtwAppBundle\Services\ElectionProvider;
+use Btw\Bundle\BtwAppBundle\Services\PartyResultsProvider;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AnalysisController extends Controller
 {
+	/** @var  ElectionProvider */
+	private $electionProvider;
+	/** @var  PartyResultsProvider */
+	private $partyResultsProvider;
+
 	public function indexAction()
 	{
-		$electionProvider = $this->get('btw_election_provider');
-		$partyResultProvider = $this->get('btw_party_results_provider');
+		$years    = $this->getElectionYears();
+		$year     = max($years);
+		$election = $this->getElectionProvider()->forYear($year);
 
-		/** ALL ELECTION YRS */
-		$elections = $electionProvider->getAll();
-		$years = array();
-		foreach ($elections as $election) {
-			$years[] = date('Y', $election->getDate()->getTimestamp());
+		$results = $this->getPartyResultsProvider()->forCountry($election);
+		usort($results, array($this, 'compareResults'));
+
+		$serialized = array_map(array($this, 'serializeResult'), $results);
+		return $this->render('BtwAppBundle:Analysis:index.html.twig', array(
+				'year'    => $year,
+				'years'   => $years,
+				'results' => $serialized)
+		);
+	}
+
+	private function getElectionYears()
+	{
+		$elections = $this->getElectionProvider()->getAll();
+
+		return array_map(function ($election) {
+			return date('Y', $election->getDate()->getTimestamp());
+		}, $elections);
+	}
+
+	private function serializeResult(PartyResult $result)
+	{
+		return array(
+			'name'  => $result->getPartyAbbreviation(),
+			'label' => $result->getPartyFullName(),
+			'color' => $result->getColor(),
+			'seats' => $result->getSeats(),
+			'y'     => $result->getSeats(),
+		);
+	}
+
+	private function compareResults(PartyResult $a, PartyResult $b)
+	{
+		$x = $a->getSeats();
+		$y = $b->getSeats();
+		return $x < $y ? 1 : ($x > $y ? -1 : 0);
+	}
+
+	/**
+	 * @return ElectionProvider
+	 */
+	private function getElectionProvider()
+	{
+		if ($this->electionProvider == null) {
+			$this->electionProvider = $this->get('btw_election_provider');
 		}
+		return $this->electionProvider;
+	}
 
-		/** LATEST ELECTION YEAR */
-		$latestElectionYear = max($years);
-
-		/** LATEST ELECTION RESULTS */
-		$latestElection = $electionProvider->forYear($latestElectionYear);
-		$latestPartySeatsResults = $partyResultProvider->forCountry($latestElection);
-		$latestResults = array();
-		foreach($latestPartySeatsResults as $result)
-		{
-			$latestResults[] = array('name' => $result->getPartyAbbreviation(), 'color' => $result->getColor(), 'y' => $result->getSeats());
+	/**
+	 * @return PartyResultsProvider
+	 */
+	private function getPartyResultsProvider()
+	{
+		if ($this->partyResultsProvider == null) {
+			$this->partyResultsProvider = $this->get('btw_party_results_provider');
 		}
-		usort($latestResults, function($result1, $result2)
-		{
-			if($result1['y'] == $result2['y']) return 0;
-			if($result1['y'] < $result2['y']) return 1;
-			return -1;
-		});
-
-		return $this->render('BtwAppBundle:Analysis:index.html.twig', array('year' => $latestElectionYear,
-			'all_years' => $years,
-			'population' => $latestResults));
+		return $this->partyResultsProvider;
 	}
 }
