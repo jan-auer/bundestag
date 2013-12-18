114fea2404227918c4dac1b7866063b567e3ebeb
centigradephilip <phschaef@googlemail.com>
2013-12-18 15:18:23 +0100

    Refined option help texts

--- misc/Benchmarking/Btw.Benchmark/Btw.Benchmark.CLI/Options.cs
+++ misc/Benchmarking/Btw.Benchmark/Btw.Benchmark.CLI/Options.cs
@@ -11,16 +11,16 @@ namespace Btw.Benchmark
         [ParserState]
         public IParserState LastParserState { get; set; }
 
-        [OptionArray('t', "time", Required = true, HelpText = "Approximate delay time between calls.")]
+        [OptionArray('t', "time", Required = true, HelpText = "Approximate delay time in milliseconds between calls. Please Specify one time per desired run.")]
         public string[] DelayTime { get; set; }
 
-        [OptionArray('n', "count", Required=true, HelpText="Total terminal count.")]
+        [OptionArray('n', "count", Required=true, HelpText="Total terminal count per run. Please specify one terminal count per desired run.")]
         public string[] TerminalCount { get; set; }
 
         [OptionArray('u', "urls", Required = true, HelpText = "URLs to call.")]
         public string[] Urls { get; set; }
 
-        [OptionArray('r', "rates", Required = true, HelpText = "Rates mapping to specified URLs")]
+        [OptionArray('r', "rates", Required = true, HelpText = "Rates mapping to specified URLs.")]
         public string[] Rates { get; set; }
 
         [HelpOption]
