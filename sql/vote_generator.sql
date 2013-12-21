2ce4db0086f1daf5019b4ab28a1decd8f40e5d9f
Manuel Gerding <me@manuel-gerding.de>
2013-12-21 08:14:03 +0100

    replacement of aggregated tables by materialized views

--- sql/vote_generator.sql
+++ sql/vote_generator.sql
@@ -7,9 +7,24 @@ INSERT INTO first_result (candidate_id)
          FROM aggregated_first_result
        ) t;
 
+DROP TABLE aggregated_first_result CASCADE;
+CREATE MATERIALIZED VIEW aggregated_first_result (candidate_id, count) AS (
+  SELECT candidate_id, COUNT(*) AS count
+  FROM first_result
+  GROUP BY candidate_id
+);
+
 INSERT INTO second_result (state_list_id, constituency_id)
   SELECT state_list_id, constituency_id
   FROM (
          SELECT state_list_id, constituency_id, generate_series(1, count)
          FROM aggregated_second_result
        ) t;
+
+DROP TABLE aggregated_second_result CASCADE;
+CREATE MATERIALIZED VIEW aggregated_second_result (state_list_id, constituency_id, count) AS (
+  SELECT state_list_id, constituency_id, COUNT(*) AS count
+  FROM second_result
+  GROUP BY state_list_id, constituency_id
+);
+--RERUN OF analysis.sql and seat_distribution.sql required (because of cascading)!!
