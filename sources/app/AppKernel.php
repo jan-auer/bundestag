bdb7ef8595887f9de9ed77643e786d41285de129
Jan Michael Auer <jan.auer@me.com>
2014-01-07 16:51:02 +0100

    Add a bootstrap extensions module.

--- sources/app/AppKernel.php
+++ sources/app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
 
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+	        new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
 
 	        new Btw\Bundle\PersistenceBundle\BtwPersistenceBundle(),
 	        new Btw\Bundle\ImporterBundle\BtwImporterBundle(),
