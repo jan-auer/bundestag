360d0155d4de5b51a8e6d5a8c2a90b4844159d7e
centigradephilip <phschaef@googlemail.com>
2014-01-08 17:50:08 +0100

    Added byId, getMyRepository

--- sources/src/Btw/Bundle/BtwAppBundle/Services/StateListProvider.php
+++ sources/src/Btw/Bundle/BtwAppBundle/Services/StateListProvider.php
@@ -14,6 +14,18 @@ use Btw\Bundle\PersistenceBundle\Entity\State;
 class StateListProvider extends AbstractProvider
 {
 
+	/** @var  EntityRepository */
+	private $repository;
+
+	/**
+	 * @param $id
+	 * @return StateList
+	 */
+	public function byId($id)
+	{
+		return $this->getMyRepository()->find($id);
+	}
+
 	/**
 	 * @param State $state
 	 *
@@ -23,5 +35,16 @@ class StateListProvider extends AbstractProvider
 	{
 		$stateList = $this->getRepository('StateList')->findBy(array('state' => $state));
 		return $stateList;
+	}
+
+	/**
+	 * @return EntityRepository
+	 */
+	private function getMyRepository()
+	{
+		if ($this->repository == null) {
+			$this->repository = $this->getRepository('StateList');
+		}
+		return $this->repository;
 	}
 } 
