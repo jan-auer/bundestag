9cb2978caed30e1fdc11de1a8c9d1eca0976ee45
Jan Michael Auer <jan.auer@me.com>
2014-01-20 18:42:49 +0100

    Add phpDoc to the LayoutController.

--- sources/src/Btw/Bundle/BtwAppBundle/Controller/LayoutController.php
+++ sources/src/Btw/Bundle/BtwAppBundle/Controller/LayoutController.php
@@ -4,10 +4,18 @@ namespace Btw\Bundle\BtwAppBundle\Controller;
 
 use Btw\Bundle\BtwAppBundle\Services\ElectionProvider;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Contains actions for all pages within the system.
+ */
 class LayoutController extends Controller
 {
 
+	/**
+	 * Renders a navigation populated with a list of recent elections.
+	 * @return Response
+	 */
 	public function electionsNavbarAction()
 	{
 		/** @var ElectionProvider $provider */
